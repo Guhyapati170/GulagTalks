e56512b03d797bf83cb398fcf7bb3ffc41bc4d7b
Guhyapati <sammyutube001@gmail.com>
2025-07-13 17:13:55 +0530

    Add files via upload

--- GulagTalks/Chat/views.py
+++ GulagTalks/Chat/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .forms import *
-from .models import *
+from .forms import ChatmessageCreateForm
+from .models import GroupChats
+
 # Create your views here.
 
 @login_required
